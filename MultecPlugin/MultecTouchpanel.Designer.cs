a59b5666a4ce82012df1eb752facfb1bb91b7235
Patrick <nph3304@gmail.com>
2018-02-22 22:14:01 +0100

    Initial commit

---
+++ MultecPlugin/MultecTouchpanel.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace MultecPlugin
+{
+    partial class MultecTouchpanel
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.but_Xplus = new System.Windows.Forms.Button();
+            this.buttonHome = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.but_step_1 = new System.Windows.Forms.Button();
+            this.but_step_10 = new System.Windows.Forms.Button();
+            this.but_step_50 = new System.Windows.Forms.Button();
+            this.but_Yplus = new System.Windows.Forms.Button();
+            this.but_Yminus = new System.Windows.Forms.Button();
+            this.but_Xminus = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // but_Xplus
+            // 
+            this.but_Xplus.Location = new System.Drawing.Point(344, 93);
+            this.but_Xplus.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.but_Xplus.Name = "but_Xplus";
+            this.but_Xplus.Size = new System.Drawing.Size(112, 82);
+            this.but_Xplus.TabIndex = 0;
+            this.but_Xplus.Text = "X+";
+            this.but_Xplus.UseVisualStyleBackColor = true;
+            this.but_Xplus.Click += new System.EventHandler(this.but_Xplus_Click);
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.Location = new System.Drawing.Point(9, 208);
+            this.buttonHome.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Size = new System.Drawing.Size(112, 35);
+            this.buttonHome.TabIndex = 0;
+            this.buttonHome.Text = "Home";
+            this.buttonHome.UseVisualStyleBackColor = true;
+            this.buttonHome.Click += new System.EventHandler(this.buttonHome_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.but_step_1);
+            this.groupBox1.Controls.Add(this.but_step_10);
+            this.groupBox1.Controls.Add(this.but_step_50);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(114, 156);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Distance";
+            // 
+            // but_step_1
+            // 
+            this.but_step_1.Location = new System.Drawing.Point(6, 107);
+            this.but_step_1.Name = "but_step_1";
+            this.but_step_1.Size = new System.Drawing.Size(95, 35);
+            this.but_step_1.TabIndex = 2;
+            this.but_step_1.Text = "1 mm";
+            this.but_step_1.UseVisualStyleBackColor = true;
+            this.but_step_1.Click += new System.EventHandler(this.but_step_1_Click);
+            // 
+            // but_step_10
+            // 
+            this.but_step_10.Location = new System.Drawing.Point(6, 66);
+            this.but_step_10.Name = "but_step_10";
+            this.but_step_10.Size = new System.Drawing.Size(95, 35);
+            this.but_step_10.TabIndex = 1;
+            this.but_step_10.Text = "10 mm";
+            this.but_step_10.UseVisualStyleBackColor = true;
+            this.but_step_10.Click += new System.EventHandler(this.but_step_10_Click);
+            // 
+            // but_step_50
+            // 
+            this.but_step_50.Location = new System.Drawing.Point(6, 25);
+            this.but_step_50.Name = "but_step_50";
+            this.but_step_50.Size = new System.Drawing.Size(95, 35);
+            this.but_step_50.TabIndex = 0;
+            this.but_step_50.Text = "50 mm";
+            this.but_step_50.UseVisualStyleBackColor = true;
+            this.but_step_50.Click += new System.EventHandler(this.but_step_50_Click);
+            // 
+            // but_Yplus
+            // 
+            this.but_Yplus.Location = new System.Drawing.Point(233, 5);
+            this.but_Yplus.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.but_Yplus.Name = "but_Yplus";
+            this.but_Yplus.Size = new System.Drawing.Size(112, 80);
+            this.but_Yplus.TabIndex = 2;
+            this.but_Yplus.Text = "Y+";
+            this.but_Yplus.UseVisualStyleBackColor = true;
+            this.but_Yplus.Click += new System.EventHandler(this.but_Yplus_Click);
+            // 
+            // but_Yminus
+            // 
+            this.but_Yminus.Location = new System.Drawing.Point(233, 185);
+            this.but_Yminus.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.but_Yminus.Name = "but_Yminus";
+            this.but_Yminus.Size = new System.Drawing.Size(112, 80);
+            this.but_Yminus.TabIndex = 3;
+            this.but_Yminus.Text = "Y-";
+            this.but_Yminus.UseVisualStyleBackColor = true;
+            this.but_Yminus.Click += new System.EventHandler(this.but_Yminus_Click);
+            // 
+            // but_Xminus
+            // 
+            this.but_Xminus.Location = new System.Drawing.Point(124, 93);
+            this.but_Xminus.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.but_Xminus.Name = "but_Xminus";
+            this.but_Xminus.Size = new System.Drawing.Size(112, 82);
+            this.but_Xminus.TabIndex = 4;
+            this.but_Xminus.Text = "Y-";
+            this.but_Xminus.UseVisualStyleBackColor = true;
+            // 
+            // MultecTouchpanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.but_Xminus);
+            this.Controls.Add(this.but_Yminus);
+            this.Controls.Add(this.but_Yplus);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonHome);
+            this.Controls.Add(this.but_Xplus);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MultecTouchpanel";
+            this.Size = new System.Drawing.Size(676, 697);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button but_Xplus;
+        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button but_step_1;
+        private System.Windows.Forms.Button but_step_10;
+        private System.Windows.Forms.Button but_step_50;
+        private System.Windows.Forms.Button but_Yplus;
+        private System.Windows.Forms.Button but_Yminus;
+        private System.Windows.Forms.Button but_Xminus;
+    }
+}
---
+++ MultecPlugin/MultecTouchpanel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RepetierHostExtender.interfaces;
+using RepetierHostExtender.geom;
+
+
+namespace MultecPlugin
+{
+    public partial class MultecTouchpanel : UserControl, IHostComponent
+    {
+
+        private IHost host;
+        public MultecTouchpanel()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Store reference to host for later use
+        /// </summary>
+        /// <param name="_host">Host instance</param>
+        public void Connect(IHost _host)
+        {
+            host = _host;
+        }
+        #region IHostComponent implementation
+
+        // Name inside component repository
+        public string ComponentName { get { return "CoolControl"; } }
+        // Name for tab
+        public string ComponentDescription { get { return "Cool Control"; } }
+        // Used for positioning.
+        public int ComponentOrder { get { return 8000; } }
+        // Where to add it. We want it on the right tab.
+        public PreferredComponentPositions PreferredPosition { get { return PreferredComponentPositions.SIDEBAR; } }
+        // Return the UserControl.
+        public Control ComponentControl { get { return this; } }
+
+        public ThreeDView Associated3DView { get { return null; } }
+
+        public void ComponentActivated()
+        {
+            //throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Button functions
+
+        private void but_Xminus_Click(object sender, EventArgs e)
+        {
+            if(host.Connection.connector.IsConnected())
+                host.Connection.injectManualCommand("G91");
+                host.Connection.injectManualCommand("G1 X"+ -step_dist);
+                host.Connection.injectManualCommand("G90");
+        }
+
+        private void but_Xplus_Click(object sender, EventArgs e)
+        {
+            if (host.Connection.connector.IsConnected())
+                host.Connection.injectManualCommand("G91");
+                host.Connection.injectManualCommand("G1 X" + step_dist);
+                host.Connection.injectManualCommand("G90");
+        }
+
+        private void but_Yplus_Click(object sender, EventArgs e)
+        {
+            if (host.Connection.connector.IsConnected())
+                host.Connection.injectManualCommand("G91");
+                host.Connection.injectManualCommand("G1 Y" + step_dist);
+                host.Connection.injectManualCommand("G90");
+        }
+
+        private void but_Yminus_Click(object sender, EventArgs e)
+        {
+            if (host.Connection.connector.IsConnected())
+                host.Connection.injectManualCommand("G91");
+                host.Connection.injectManualCommand("G1 Y" + -step_dist);
+                host.Connection.injectManualCommand("G90");
+        }
+
+        private void buttonHome_Click(object sender, EventArgs e)
+        {
+            if (host.Connection.connector.IsConnected())
+                host.Connection.injectManualCommand("G28");
+        }
+
+        #endregion
+
+        private double step_dist;
+
+        private void but_step_50_Click(object sender, EventArgs e)
+        {
+            step_dist = 50;
+        }
+
+        private void but_step_10_Click(object sender, EventArgs e)
+        {
+            step_dist = 10;
+        }
+
+        private void but_step_1_Click(object sender, EventArgs e)
+        {
+            step_dist = 1;
+        }
+
+
+    }
+}
